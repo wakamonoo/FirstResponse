d8a26e6266aab2bbdbbe26154f986a4266b85a84
batallerj <jovenserdan.bataller@bicol-u.edu.ph>
2024-08-24 23:49:08 +0800

    updates

---
+++ FirstResponse/app/src/main/java/com/example/android/firstaidanddisastermanagementapp/SplashScreenActivity.kt
@@ -0,0 +1,25 @@
+package com.example.android.firstaidanddisastermanagementapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.widget.Button;
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+
+        supportActionBar?.hide()
+
+        // Add button click listener here
+        val startButton = findViewById<Button>(R.id.startButton)  // Replace with your button's id
+        startButton.setOnClickListener {
+            val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
