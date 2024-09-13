d5656cea95ecb1c9336305f2070dbbe2f93ded55
batallerj <jovenserdan.bataller@bicol-u.edu.ph>
2024-09-13 19:51:18 +0800

    up

--- FirstResponse/app/src/main/java/com/example/android/firstresponse/FirstAidKit.kt
+++
@@ -1,9 +0,0 @@
-package com.example.android.firstresponse
-
-data class FirstAidKit(
-    val imageResId: Int,
-    val title: String,
-    val descriptionResId: Int, // Change to Int for resource ID
-    val buttonText: String
-)
-
