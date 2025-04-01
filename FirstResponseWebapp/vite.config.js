f6b554db48ed37614f82d1a8d9651da3747acfa7
wakamonoo <joven.serdanbataller21@gmail.com>
2025-04-01 18:27:29 +0800

    update

--- FirstResponseWebapp/vite.config.js
+++ FirstResponseWebapp/vite.config.js
@@ -1,18 +1,23 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
+
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ["resources/sass/app.scss", "resources/js/app.js"],
-            refresh: true,
-        }),
-        vue(),
-    ],
-    resolve: {
-        alias: {
-            "@": "/resources/js",
-            vue: "vue/dist/vue.esm-bundler.js",
-        },
+  plugins: [
+    laravel({
+      input: ["resources/sass/app.scss", "resources/js/app.js"],
+      refresh: true,
+    }),
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      "@": "/resources/js",
+      vue: "vue/dist/vue.esm-bundler.js",
     },
+  },
+  build: {
+    manifest: true, // Ensure manifest is generated
+    outDir: 'public/build', // Ensure this matches Laravel's default path
+  },
 });
